272dd8f75586f4f0a8dd6162ccb0b31aa1cd14a8
Jayeon Hwang <jayeon512@gmail.com>
2025-05-08 11:12:21 -0500

    update scroll button

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+
+const header = document.querySelector('.header');
+
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +22,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,4 +35,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+
+//// cookie message
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = 'we use cookies for improved functionality and analystics.<button class ="btn btn--close-cookie">Got it!</button>';
+header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  message.parentElement.removeChild(message);
+});
+
+
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//
+const btnScrollTo = document.querySelector('.btn--scroll-to'
+);
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset);
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+
+});
+
 
