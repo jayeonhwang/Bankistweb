ed67de7d690506d1b237fcd001375d0ed4414d80
Jayeon Hwang <jayeon512@gmail.com>
2025-05-12 15:43:35 -0500

    add menu fade animation

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-
 const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to'
+);
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -59,9 +65,7 @@ message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +
 document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 //
-const btnScrollTo = document.querySelector('.btn--scroll-to'
-);
-const section1 = document.querySelector('#section--1');
+
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
@@ -136,9 +140,7 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 
 // tabbed component 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+
 
 // tabs.forEach(t => t.addEventListener('click', () => console.log('Tab')));
 
@@ -159,3 +161,21 @@ tabsContainer.addEventListener('click', function (e) {
 
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+/// Menu fade animation
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
