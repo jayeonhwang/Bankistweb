c5754b4cc75d587502595ce3efdd25c51d79f125
Jayeon Hwang <jayeon512@gmail.com>
2025-05-14 20:21:26 -0500

    lazy loading images

--- script.js
+++ script.js
@@ -226,13 +226,15 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
-  if (!entry.isIntersecting) return;
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
+
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
@@ -244,4 +246,24 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+// lazy loading images 
+const imgTargets = document.querySelectorAll('img[data-src');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry)
 
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
